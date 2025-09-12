2c1cd78a3a1d47d46c8baec1008b9edce398358f
niloy <chowdhuryniloy819@gmail.com>
2025-09-12 20:44:05 +0600

    designed the frontend part for login and register and connected with mongodb atlas

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/auth/login", { email, password });
+      alert("Login Successful");
+      localStorage.setItem("token",res.data.token);
+    } catch (error) {
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
   return (
     <div>
       <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="Email" /> <br />
-        <input type="password" placeholder="Password" /> <br />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />{" "}
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+        <br />
         <button type="submit">Login</button>
       </form>
     </div>
   );
 }
+
 export default Login;
