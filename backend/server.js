a44fe54ff73a60782d6cf431af607a9a60513603
niloy <chowdhuryniloy819@gmail.com>
2025-10-11 17:47:38 +0600

    added the contact api at server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import connectDB from "./config/db.js"
 import authRoutes from "./routes/authRoutes.js"
 import complaintRoutes from "./routes/complaintRoutes.js"
 import analyticsRoute from "./routes/analyticsRoute.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -23,6 +25,7 @@ app.get("/",(req,res)=>{
 app.use("/api/auth",authRoutes);
 app.use("/api/complaints",complaintRoutes);
 app.use("/api/analytics", analyticsRoute);
+app.use("/api/contact", contactRoutes);
 
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>console.log(`Server running on  http://localhost:${PORT}`));
