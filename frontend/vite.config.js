d09d3c66790845e5859b5de75f4a8fd1d3c790d3
niloy <chowdhuryniloy819@gmail.com>
2026-02-12 17:45:23 +0600

    fixed issue caused by .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5001",  // ✅ forward /api requests to backend
+      "/api": "http://localhost:5000",  // ✅ forward /api requests to backend
     },
   },
 });
