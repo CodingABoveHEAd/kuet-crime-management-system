8c4ed33442d9c20b1eb657f0959f7da2a95883d2
niloy <chowdhuryniloy819@gmail.com>
2026-02-12 17:58:18 +0600

    updated the main.js file to adapt changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './styles/theme.css'  // Import theme variables first
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthProvider>
-    
+  <ThemeProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </ThemeProvider>
 )
